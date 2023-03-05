41ada02a07cf8032d0f7f14ed07b138c11a03cb4
1i9h7_b1u3 <1012796366@qq.com>
2023-03-05 18:51:39 +0800

    第 5 题

--- src/theole1p5.java
+++ src/theole1p5.java
@@ -1,11 +1,12 @@
-/* 5．编写一个Java应用程序，输出全部的希腊字母。
- *
+/*
+ * 5．编写一个Java应用程序，输出全部的希腊字母。
  * 提示：找到第一个希腊字母α在Unicode中的编码，最后一个希腊字母ω在Unicode中的编码。然后循环输出这个范围内所有的字符，就得到了希腊字母表
  */
 
 public class theole1p5 {
     public static void main(String[] args) {
         for (int i = 945; i <= 969; ++i) {
+            // 注释看题目提示即可
             System.out.println((char)i);
         }
     }
