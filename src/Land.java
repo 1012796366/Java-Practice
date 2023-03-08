4bbca526b73afc494a5ae94f5160a673a1c4a472
1i9h7_b1u3 <1012796366@qq.com>
2023-03-08 15:29:30 +0800

    实验二：第 4 题

---
+++ src/Land.java
@@ -0,0 +1,10 @@
+public class Land {
+    public static void main(String[] args) {
+        Village vil1 = new Village(300, "张村");
+        Village vil2 = new Village(200, "李村");
+
+        System.out.println(Village.getWaterAmount());
+        vil1.setWaterAmount(Village.getWaterAmount() + 200);
+        System.out.println(vil2.getWaterAmount());
+    }
+}
---
+++ src/Village.java
@@ -0,0 +1,21 @@
+public class Village {
+    private static int waterAmount;
+    private int peopleNumber;
+    private String name;
+
+    public Village() {
+        peopleNumber = 0;
+        name = null;
+    }
+    public Village(int peopleNumber, String name) {
+        this.peopleNumber = peopleNumber;
+        this.name = name;
+    }
+    public void setPeopleNumber(int peopleNumber) { this.peopleNumber = peopleNumber; }
+    public int getPeopleNumber() { return this.peopleNumber; }
+    public void setName(String name) { this.name = name; }
+    public String getName() { return this.name; }
+    public static void setWaterAmount(int amount) { waterAmount = amount; }
+    public static int getWaterAmount() { return waterAmount; }
+
+}
