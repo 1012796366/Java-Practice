1cfaa2a035ab03ae2e79e90b15ffeb60fbae8472
1i9h7_b1u3 <1012796366@qq.com>
2023-03-10 19:27:05 +0800

    实验二：第 5 题

---
+++ src/polygon/test/Test.java
@@ -0,0 +1,12 @@
+package polygon.test;
+import polygon.def.RegularPolygon;
+public class Test {
+    public static void main(String[] args) {
+        RegularPolygon rp1 = new RegularPolygon();
+        RegularPolygon rp2 = new RegularPolygon(6, 4);
+        RegularPolygon rp3 = new RegularPolygon(10, 4, 5.6, 7.8);
+        System.out.println("第1个多边形周长：" + rp1.getPerimeter() + "\n第1个多边形面积：" + rp1.getArea());
+        System.out.println("第2个多边形周长：" + rp2.getPerimeter() + "\n第2个多边形面积：" + rp2.getArea());
+        System.out.println("第3个多边形周长：" + rp3.getPerimeter() + "\n第3个多边形面积：" + rp3.getArea());
+    }
+}
