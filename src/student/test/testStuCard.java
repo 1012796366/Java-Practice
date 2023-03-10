4471ef40350beeb4cea92e7a7fb8f456b5df46f6
1i9h7_b1u3 <1012796366@qq.com>
2023-03-10 19:55:43 +0800

    实验二：第 2 题

--- src/student/test/testStuCard.java
+++ src/student/test/testStuCard.java
@@ -7,7 +7,7 @@ public class testStuCard {
         StuCard stuFI = new StuCard("7608020622", "李四", "男"
                                     ,"量子计算机", 3, "木卫二");         // Full Info   包含全部信息
 
-        System.out.print("缺乏任何信息的学生：");
+        System.out.print("缺乏任何信息的学生：\n");
         stuNI.show();
         stuNI.setID("7608020311");
         stuNI.setName("王五");
@@ -15,18 +15,18 @@ public class testStuCard {
         stuNI.setMajor("宇宙机");
         stuNI.setGrade(2);
         stuNI.setLocation("火卫一");
-        System.out.print("[修改后]缺乏任何信息的学生：");
+        System.out.print("[修改后]缺乏任何信息的学生：\n");
         stuNI.show();
 
-        System.out.print("只含必要信息的学生：");
+        System.out.print("只含必要信息的学生：\n");
         stuLI.show();
         stuLI.setMajor("核弹制造工程");
         stuLI.setGrade(4);
         stuLI.setLocation("火卫一");
-        System.out.print("[修改后]只含必要信息的学生：");
+        System.out.print("[修改后]只含必要信息的学生：\n");
         stuLI.show();
 
-        System.out.print("包含全部信息的学生：");
+        System.out.print("包含全部信息的学生：\n");
         stuFI.show();
     }
 }
