1190fbb04eaa9fdecef2f1f3ec275035ddb54fbd
1i9h7_b1u3 <1012796366@qq.com>
2023-03-10 19:51:01 +0800

    实验二：第 4 题

--- src/well/Land.java
+++ src/well/Land.java
@@ -4,7 +4,7 @@ public class Land {
         Village vil1 = new Village(300, "张村");
         Village vil2 = new Village(200, "李村");
 
-        System.out.print(Village.getWaterAmount());
+        System.out.print(Village.getWaterAmount() + "\n");
         vil1.setWaterAmount(Village.getWaterAmount() + 200);
         System.out.print(vil2.getWaterAmount());
     }
