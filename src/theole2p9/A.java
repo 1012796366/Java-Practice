55b2d7fb4bfe1fdf8c54e6f324a1f2ec7c86a436
1i9h7_b1u3 <1012796366@qq.com>
2023-03-13 14:12:33 +0800

    实验二：第 9 题

---
+++ src/theole2p9/A.java
@@ -0,0 +1,20 @@
+package theole2p9;
+class A{
+
+    static int x = 100;
+
+    static long y = 200;
+
+    public void f(){
+
+        y = 300;
+
+    }
+
+    public static void g(){
+
+        x = -23;
+
+    }
+
+}
