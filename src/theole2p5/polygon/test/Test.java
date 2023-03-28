35eb42b4658a1e354a956e08944723d3717b0732
1i9h7_b1u3 <1012796366@qq.com>
2023-03-28 09:01:28 +0800

    实验二：第 5 题

---
+++ src/theole2p5/polygon/test/Test.java
@@ -0,0 +1,13 @@
+package theole2p5.polygon.test;
+import theole2p5.polygon.def.RegularPolygon;
+
+public class Test {
+    public static void main(String[] args) {
+        RegularPolygon rp1 = new RegularPolygon();
+        RegularPolygon rp2 = new RegularPolygon(6, 4);
+        RegularPolygon rp3 = new RegularPolygon(10, 4, 5.6, 7.8);
+        System.out.print("第1个多边形周长：" + rp1.getPerimeter() + "\n第1个多边形面积：" + rp1.getArea() + '\n');
+        System.out.print("第2个多边形周长：" + rp2.getPerimeter() + "\n第2个多边形面积：" + rp2.getArea() + '\n');
+        System.out.print("第3个多边形周长：" + rp3.getPerimeter() + "\n第3个多边形面积：" + rp3.getArea() + '\n');
+    }
+}
