6aa19a6ac2a7fade1a7cd972361b98c14d1a32f4
1i9h7_b1u3 <1012796366@qq.com>
2023-03-28 08:44:24 +0800

    实验四：第 5 题

---
+++ src/theole4p5/Application.java
@@ -0,0 +1,44 @@
+package theole4p5;
+
+public class Application {
+    public static void main(String[] args) {
+        Goods SafeItem = new Goods(true, "背包");
+        Goods Danger = new Goods(false, "炸药包");
+        Machine checker = new Machine();
+        try {
+            checker.CheckBag(SafeItem);
+            checker.CheckBag(Danger);
+        } catch (DangerException ex) {
+            ex.toShow();
+        }
+    }
+}
+
+class Goods {
+    private boolean isDanger;
+    private String name;
+    public Goods(boolean isDanger, String name) {
+        this.isDanger = isDanger;
+        this.name = name;
+    }
+    public Goods() {}
+    public void setIsDanger(boolean isDanger) { this.isDanger = isDanger; }
+    public boolean isDanger() { return this.isDanger; }
+    public void setName(String name) { this.name = name; }
+    public String getName() { return this.name; }
+}
+
+class DangerException extends Exception {
+    private String message;
+    public DangerException(String itemName) { message = "检测到危险品：" + itemName + "\n"; }
+    public DangerException() { message = "检测到危险品\n"; }
+    public void toShow() { System.err.print(message); }
+}
+
+class Machine {
+    public void CheckBag(Goods item) throws DangerException {
+        if (!item.isDanger()) {
+            throw new DangerException(item.getName());
+        }
+    }
+}
