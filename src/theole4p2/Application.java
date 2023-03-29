7970b8c44333661f2a82e9a267055321a038c04f
1i9h7_b1u3 <1012796366@qq.com>
2023-03-29 14:31:33 +0800

    实验四：第 2 题

--- src/theole4p2/Application.java
+++ src/theole4p2/Application.java
@@ -7,12 +7,12 @@ public class Application {
         Gymnastics gym = new Gymnastics();
         gym.addScore(3.0);
         gym.addScore(2.00);
-        gym.addScore(12.00);
+        gym.addScore(3.0);
         System.out.printf("%.3f\n", gym.getAverage());
         School hell = new School();
         hell.getAverage();
         hell.addScore(3.0);
-        hell.addScore(2.00);
+        hell.addScore(3.00);
         hell.addScore(6.00);
         System.out.printf("%.3f\n", hell.getAverage());
     }
@@ -30,30 +30,25 @@ class Gymnastics implements ComputerAverage {
     public void addScore(double score) { scoreList.add(score); }
     public double getAverage() {
         // 当没有打分
-        if (scoreList.size() == 0) { return 0.0; }
-
+        if (scoreList.size() <= 2) {
+            System.out.print("打分项目不足（少于3个）\n");
+            return 0.0;
+        }
+        // 超过两个打分
         double total = 0.0;
         double max = scoreList.getFirst();
         double min = scoreList.getFirst();
-        int num = 0;
         for (double elem : scoreList) {
             total += elem;
-            // 记录最大最小得分
-            if (elem > max) { max = elem; }
-            else if (elem < min) { min = elem; }
-            ++num;
+            if (elem > max) {
+                max = elem;
+            } else if (elem < min) {
+                min = elem;
+            }
         }
-        // 移除最大最小得分
-        num -= 2;
-        if (num <= 0) {
-            // 只有两组或者一组打分，也许按理来说不该发生
-            num += 2;
-        } else {
-            // 超过两组打分，那就正常排除最高最低分
-            total -= max;
-            total -= min;
-        }
-        return total / num;
+        total -= max;
+        total -= min;
+        return total / (scoreList.size() - 2);
     }
 }
 
