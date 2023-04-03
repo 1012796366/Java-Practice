3658a0c5f125646f355cd872bad0d745cbbca138
1i9h7_b1u3 <1012796366@qq.com>
2023-04-03 15:10:16 +0800

    实验五：第 2 题

--- src/theole5p2/Application.java
+++ src/theole5p2/Application.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("编码最大的字符：" + charProcessor.proceed(reader.nextLine()));
+        System.out.println("编码最大的字符：" + charProcessor.getMAX(reader.nextLine()));
     }
 }
 
 class charProcessor {
-    static char proceed(String input) {
+    static char getMAX(String input) {
         char max = input.charAt(0);
         for (int i = 1; i < input.length(); ++i) {
             if (input.charAt(i) > max) {
