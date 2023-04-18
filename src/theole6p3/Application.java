4300f322f3d16a91e3a4eafced17b77c5e3a10d0
1i9h7_b1u3 <1012796366@qq.com>
2023-04-18 08:33:35 +0800

    实验六：第 3 题

---
+++ src/theole6p3/Application.java
@@ -0,0 +1,24 @@
+package theole6p3;
+import java.io.*;
+
+public class Application {
+    public static void main(String[] args) {
+        try {
+            BufferedReader input = new BufferedReader(new FileReader("src/theole6p3/score.txt"));
+            BufferedWriter output = new BufferedWriter(new FileWriter("src/theole6p3/totalscoretxt"));
+            String s;
+            String score;
+            String[] scr;
+
+            while ((s = input.readLine()) != null) {
+                scr = s.split("\\D+");
+                score = Integer.toString(Integer.parseInt(scr[1]) + Integer.parseInt(scr[2]) + Integer.parseInt(scr[3]));
+                output.write(s.substring(0, s.indexOf("，")) + "，总分" + score + "分。\n");
+            }
+            input.close();
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
