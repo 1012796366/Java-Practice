0c3f0fb15cc3cedb03a25c79091f83a179307e60
1i9h7_b1u3 <1012796366@qq.com>
2023-04-18 09:25:08 +0800

    实验六：第 5 题

--- src/theole6p5/Application.java
+++ src/theole6p5/Application.java
@@ -1,13 +1,21 @@
 package theole6p5;
 import java.io.*;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         try {
+            Scanner reader = new Scanner(System.in);
             File picDelete = new File("C:/newFile/pic.jpg");
             File dirCheck = new File("D:/newFile");
             if (!dirCheck.exists() && !dirCheck.isDirectory()) {
-                dirCheck.mkdirs();
+                while (!dirCheck.mkdirs()) {
+                    System.err.print("文件夹创建失败，请检查文件夹是否被占用或者是否拥有权限。\n");
+                    System.out.print("是否重试创建文件夹？（Y/n）\n");
+                    if (!reader.next().equals("Y")) {
+                        System.exit(1);
+                    }
+                }
             }
             BufferedInputStream picInput = new BufferedInputStream(new FileInputStream("C:/newFile/pic.jpg"));
             BufferedOutputStream picOutput = new BufferedOutputStream(new FileOutputStream("D:/newFile/pic.jpg"));
@@ -17,6 +25,7 @@ public class Application {
             while ((s = fileInput.readLine()) != null) {
                 System.out.print(s);
             }
+            System.out.print("\n");
             fileInput.close();
 
             // 图片剪切
@@ -24,9 +33,16 @@ public class Application {
             while ((ch = picInput.read()) != -1) {
                 picOutput.write(ch);
             }
-            picDelete.delete();
             picInput.close();
             picOutput.close();
+
+            while (!picDelete.delete()) {
+                System.err.print("图片文件删除失败，请检查文件是否被占用或者是否拥有权限。\n");
+                System.err.print("是否重试删除？（Y/n）\n");
+                if (!reader.next().equals("Y")) {
+                    System.exit(1);
+                }
+            }
 
         } catch (IOException e) {
             System.err.print("捕获异常！\n");
