2e29d15ed8c6205ac30d2f0d7282a4cbb122b029
1i9h7_b1u3 <1012796366@qq.com>
2023-04-19 22:50:09 +0800

    实验七：第 4 题

---
+++ src/theole7p4/Application.java
@@ -0,0 +1,55 @@
+package theole7p4;
+import java.sql.*;
+
+public class Application {
+    public static void main(String[] args) {
+        SQLInteractive sqlobj = new SQLInteractive("jdbc:mysql://[数据库地址，本机上搭建一般为localhost，写的时候去掉中括号]:[数据库端口，一般不改就是3306]/[数据库名，该库必须存在]?useSSL=false&serverTimezone=UTC",
+                "[账户名，没添加的话就是root]", "[账户密码]");
+        sqlobj.insert();
+        sqlobj.delete();
+        sqlobj.modify();
+        sqlobj.query();
+    }
+}
+
+class SQLInteractive {
+    public Connection con = null;
+    public Statement status;
+    public ResultSet result;
+    public SQLInteractive(String url, String username, String password) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, username, password);
+            status = con.createStatement();
+        } catch (Exception e) {
+            System.err.print("第一次捕获异常！\n");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+    public void insert() {
+        try {
+            status.execute("INSERT INTO student(stuID, stuName, courseName, score) VALUES('123456', '张三', 'Java', '90')");
+            status.execute("INSERT INTO student(stuID, stuName, courseName, score) VALUES('654321', '李四', 'Java', '80')");
+            status.execute("INSERT INTO student(stuID, stuName, courseName, score) VALUES('321456', '王五', 'Java', '95')");
+        } catch (SQLException e) {}
+    }
+    public void delete() {
+        try {
+            int res2 = status.executeUpdate("DELETE FROM student WHERE stuName='李四'");
+        } catch (SQLException e) {}
+    }
+    public void modify() {
+        try {
+            int res3 = status.executeUpdate("UPDATE student SET score=100 WHERE stuName='张三'");
+        } catch (SQLException e) {}
+    }
+    public void query() {
+        try {
+            ResultSet res4 = status.executeQuery("SELECT MAX(score) FROM student WHERE courseName='Java'");
+            while (res4.next()) {
+                System.out.print(res4.getInt(1));
+            }
+        } catch (SQLException e) {}
+    }
+}
